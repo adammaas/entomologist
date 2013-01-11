d737e988a2cea49abcdec939b5a5fae02f9eb6d4
Adam <adam@ventureinteractive.com>
2013-01-11 13:31:31 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# ENTOMOLOGIST v2
+
+This is an application for project management
+[*Venture Interactive*](http://www.ventureinteractive.com)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Entomologist::Application.config.secret_token = '763ec2d95465a1af2bd169f4eef01e1f3624651870bd768843f8fc6aad7b75628085441ef6e24514d05cd11d5ddeeae15d8aa0d81c7f8efccfe616fec2b30ee0'
