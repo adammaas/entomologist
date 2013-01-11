fb106594be7f43d3c123ef0369d7adaefbb01c40
Adam <adam@ventureinteractive.com>
2013-01-11 15:42:51 -0500

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Entomologist::Application.routes.draw do
-  get "static_pages/home"
+  root to: 'static_pages#home'
 
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,13 +5,18 @@ describe "Static pages" do
   describe "Home page" do
     
     it "should have the h1 'ENTOMOLOGIST'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('h1', :text => 'ENTOMOLOGIST')
     end
     
     it "should have the title 'Home'" do
-      visit '/static_pages/home'
-      page.should have_selector('title', :text => 'ENTOMOLOGIST | Home')
+      visit root_path
+      page.should have_selector('title', :text => 'ENTOMOLOGIST')
+    end
+    
+    it "should not have a custom page title" do
+      visit root_path
+      page.should_not have_selector('title', :text => '| Home')
     end
   end
 end
